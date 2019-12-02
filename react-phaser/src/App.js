867088b8d91066910815ea41289c3cac284cabdd
Da-Colon <dcolonantonio@gmail.com>
2019-12-02 00:36:52 -0500

    added signup form and connected to database

--- react-phaser/src/App.js
+++ react-phaser/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Route} from 'react-router-dom'
 import './App.css';
 import Home from './components/Home';
+import Signup from './components/Home/signup';
 
 function App() {
   return (
     <div className="App">
-      <Route path='/' component={Home}/>
+      <Route path='/' component={Home} exact />
+      <Route path='/signup' component={Signup} exact />
     </div>
   );
 }
---
+++ react-phaser/src/components/Home/signup/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SignupForm from './signupForm'
+
+export default function Signup() {
+  return (
+    <div>
+      <SignupForm />
+    </div>
+  )
+}
