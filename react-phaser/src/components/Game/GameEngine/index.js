45f01430a64df2663606f30f8d67742bba25f249
Da-Colon <dcolonantonio@gmail.com>
2019-12-05 22:24:00 -0500

    added collision and depth to layers

--- react-phaser/src/components/Game/GameEngine/index.js
+++ react-phaser/src/components/Game/GameEngine/index.js
@@ -1,25 +1,26 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Phaser from "phaser";
 import io from "socket.io-client";
-import store from '../../../store'
+import store from "../../../store";
 
 // import map assets
 import spriteSheet from "../assets/Maps/ff6-tiles.png";
-import jsonMap from "../assets/Maps/01-start.json";
+import jsonMap from "../assets/Maps/map01New.json";
 
 // import player sprites
-import mog from '../assets/PlayerSprites/Mog.png'
-import celes from '../assets/PlayerSprites/Celes.png'
-import cyan from '../assets/PlayerSprites/Cyan.png'
-import locke from '../assets/PlayerSprites/Locke.png'
-import relm from '../assets/PlayerSprites/Relm.png'
-import terra from '../assets/PlayerSprites/Terra.png'
+import mog from "../assets/PlayerSprites/Mog.png";
+import celes from "../assets/PlayerSprites/Celes.png";
+import cyan from "../assets/PlayerSprites/Cyan.png";
+import locke from "../assets/PlayerSprites/Locke.png";
+import relm from "../assets/PlayerSprites/Relm.png";
+import terra from "../assets/PlayerSprites/Terra.png";
 
+// import enemy sprites
+import atma from "../assets/EnemySprites/Atma.gif";
 
 export default function GameEngine() {
-
-const userInfo = store.getState().user
-const [user] = useState(userInfo)
+  const userInfo = store.getState().user;
+  const [user] = useState(userInfo);
 
   class BootScene extends Phaser.Scene {
     constructor() {
@@ -36,23 +37,25 @@ const [user] = useState(userInfo)
       // Loads Players
       function setPlayerAvatar() {
         switch (user.avatar) {
-          case '1':
-            return locke
-          case '2':
-            return celes
-          case '3':
-            return cyan
-          case '4':
-            return terra
-          case '5':
-            return relm
-          case 'admin':
-            return mog
-            default:
-              return console.log("ERROR: You must have be logged in")
+          case "1":
+            return locke;
+          case "2":
+            return celes;
+          case "3":
+            return cyan;
+          case "4":
+            return terra;
+          case "5":
+            return relm;
+          case "admin":
+            return mog;
+          default:
+            return console.log("ERROR: You must have be logged in");
         }
       }
       this.textures.addBase64("player", setPlayerAvatar());
+
+      this.load.image("boss", atma);
     }
     create() {
       this.scene.start("WorldScene");
@@ -66,12 +69,14 @@ const [user] = useState(userInfo)
       });
     }
     create() {
-      const endpoint = "http://localhost:3000"
+      const endpoint = "http://localhost:3000";
 
       this.socket = io(endpoint);
       this.otherPlayers = this.physics.add.group();
 
       this.createMap();
+
+      this.createEnemies();
 
       this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -100,30 +105,26 @@ const [user] = useState(userInfo)
       this.socket.on(
         "disconnect",
         function(playerId) {
-          this.otherPlayers.getChildren().forEach(
-            function(player) {
-              if (playerId === player.playerId) {
-                player.destroy();
-              }
+          this.otherPlayers.getChildren().forEach(function(player) {
+            if (playerId === player.playerId) {
+              player.destroy();
             }
-          );
+          });
         }.bind(this)
       );
 
       this.socket.on(
         "playerMoved",
         function(playerInfo) {
-          this.otherPlayers.getChildren().forEach(
-            function(player) {
-              if (playerInfo.playerId === player.playerId) {
-                player.flipX = playerInfo.flipX;
-                player.setPosition(playerInfo.x, playerInfo.y);
-              }
+          this.otherPlayers.getChildren().forEach(function(player) {
+            if (playerInfo.playerId === player.playerId) {
+              player.flipX = playerInfo.flipX;
+              player.setPosition(playerInfo.x, playerInfo.y);
             }
-          );
+          });
         }.bind(this)
       );
-      }
+    }
 
     createMap() {
       this.map = this.make.tilemap({
@@ -132,8 +133,11 @@ const [user] = useState(userInfo)
 
       let tiles = this.map.addTilesetImage("ff6-tiles", "tiles", 32, 32);
 
-      this.map.createStaticLayer("Tile Layer 1", tiles, 0, 0);
+      this.map.createStaticLayer("ground", tiles, 0, 0);
+      this.house = this.map.createStaticLayer("house", tiles, 0, 0).setCollisionByExclusion([-1]);
+      this.treeTops = this.map.createStaticLayer("tree tops", tiles, 0, 0).setDepth(2);
 
+      
       this.physics.world.bounds.width = this.map.widthInPixels;
       this.physics.world.bounds.height = this.map.heightInPixels;
     }
@@ -151,10 +155,16 @@ const [user] = useState(userInfo)
 
       this.container.body.setCollideWorldBounds(true);
       this.physics.add.collider(this.container, this.spawns);
+      this.physics.add.collider(this.container, this.house);
     }
 
     addOtherPlayers(playerInfo) {
-      const otherPlayer = this.add.sprite(playerInfo.x, playerInfo.y, 'player', 9);
+      const otherPlayer = this.add.sprite(
+        playerInfo.x,
+        playerInfo.y,
+        "player",
+        9
+      );
       otherPlayer.setTint(Math.random() * 0xffffff);
       otherPlayer.playerId = playerInfo.playerId;
       this.otherPlayers.add(otherPlayer);
@@ -172,32 +182,108 @@ const [user] = useState(userInfo)
       this.cameras.main.roundPixels = true; // avoid tile bleed
     }
 
+    createEnemies() {
+      // where the enemies will be
+      this.spawns = this.physics.add.group({
+        classType: Phaser.GameObjects.Sprite
+      });
+      for (var i = 0; i < 5; i++) {
+        const location = this.getValidLocation();
+        // parameters are x, y, width, height
+        var enemy = this.spawns.create(
+          location.x,
+          location.y,
+          this.getEnemySprite()
+        );
+        enemy.body.setCollideWorldBounds(true);
+        enemy.body.setImmovable();
+      }
+
+      // move enemies
+      this.timedEvent = this.time.addEvent({
+        delay: 3000,
+        callback: this.moveEnemies,
+        callbackScope: this,
+        loop: true
+      });
+    }
+
+    moveEnemies() {
+      this.spawns.getChildren().forEach(enemy => {
+        const randNumber = Math.floor(Math.random() * 4 + 2);
+
+        switch (randNumber) {
+          case 1:
+            enemy.body.setVelocityX(50);
+            break;
+          case 2:
+            enemy.body.setVelocityX(-50);
+            break;
+          case 3:
+            enemy.body.setVelocityY(50);
+            break;
+          case 4:
+            enemy.body.setVelocityY(50);
+            break;
+          default:
+            enemy.body.setVelocityX(50);
+        }
+      });
+
+      setTimeout(() => {
+        this.spawns.setVelocityX(0);
+        this.spawns.setVelocityY(0);
+      }, 500);
+    }
+
+    getEnemySprite() {
+      var sprites = ["boss"];
+      return sprites[Math.floor(Math.random() * sprites.length)];
+    }
+
+    getValidLocation() {
+      var validLocation = false;
+      var x, y;
+      while (!validLocation) {
+        x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+        y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+
+        var occupied = false;
+        this.spawns.getChildren().forEach(child => {
+          if (child.getBounds().contains(x, y)) {
+            occupied = true;
+          }
+        });
+        if (!occupied) validLocation = true;
+      }
+      return { x, y };
+
+    }
+
     update() {
       if (this.container) {
         this.container.body.setVelocity(0);
 
         // Horizontal movement
         if (this.cursors.left.isDown) {
-          this.container.body.setVelocityX(-80);
+          this.container.body.setVelocityX(-100);
         } else if (this.cursors.right.isDown) {
-          this.container.body.setVelocityX(80);
+          this.container.body.setVelocityX(100);
         }
 
         // Vertical movement
         if (this.cursors.up.isDown) {
-          this.container.body.setVelocityY(-80);
+          this.container.body.setVelocityY(-100);
         } else if (this.cursors.down.isDown) {
-          this.container.body.setVelocityY(80);
+          this.container.body.setVelocityY(100);
         }
 
         // Update the animation last and give left/right animations precedence over up/down animations
         if (this.cursors.left.isDown) {
           this.player.anims.play("left", true);
           this.player.flipX = true;
-
         } else if (this.cursors.right.isDown) {
           this.player.anims.play("right", true);
-
         } else if (this.cursors.up.isDown) {
           this.player.anims.play("up", true);
         } else if (this.cursors.down.isDown) {
@@ -247,12 +333,5 @@ const [user] = useState(userInfo)
 
   var game = new Phaser.Game(config);
 
-  
-  return (
-    <>
-    
-    </>
-  );
+  return <></>;
 }
-
-
